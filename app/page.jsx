c00d0b21cd88131e4cba08f3eab291212ee324db
Rafid13iit <rafidssrr.6767@gmail.com>
2024-09-04 12:37:28 +0600

    next.config.mjs deleted

---
+++ app/page.jsx
@@ -0,0 +1,24 @@
+import Feed from '@components/Feed'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover, Create & Share 
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">
+           AI-Powered Prompts
+        </span>
+      </h1>
+      <p className="desc text-center">
+        Prompts Master is an open-source AI tool designed for the modern world, enabling users to discover, create, and share innovative prompts, driving creativity and collaboration
+      </p>
+
+      <Feed />
+
+    </section>
+  )
+}
+
+export default Home
